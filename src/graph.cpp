003a4b134b636c7a6db9417143d5b064eb8a8fbb
Heiko-E <hegerter@posteo.de>
2023-02-12 16:35:26 +0100

    add graph import and spanning tree output

--- src/graph.cpp
+++ src/graph.cpp
@@ -3,6 +3,191 @@
 #include <map>
 #include <bits/stdc++.h>
 using namespace std;
+template <class T>
+class PriorityQueue
+/// @brief The value of the PriorityQueue is to always have access to the vertex
+///        with the next shortest link in the shortest path calculation at the top of the queue.
+
+{
+public:
+  PriorityQueue() {}
+
+  ~PriorityQueue() {}
+
+  bool setMinPrioirity(const T &queueElement, int priority)
+  {
+    /// @brief changes the priority and path of a queue element to the minimum.
+    /// @param queueElement the element to change
+    /// @param priority the new priority
+    /// @return true if the update succeeded
+
+    typename list<pair<T, int>>::iterator it;
+    for (it = this->queue_.begin(); it != this->queue_.end(); it++)
+    {
+      if (it->first == queueElement)
+      {
+        if (it->second > priority)
+        {
+          it->second = priority;
+          it->first = queueElement;
+          this->queue_.sort([](pair<T, int> a, pair<T, int> b)
+                            { return a.second < b.second; });
+          return true;
+        }
+        return false;
+      }
+    }
+    return false;
+  }
+
+  int minPrioirty()
+  {
+    /// @brief priority of the top element of the queue.
+    /// @return the pririty of the top element.
+    return this->queue_.front().second;
+  }
+
+  bool contains(const T &queueElement)
+  {
+    /// @brief does the queue contains the element.
+    /// @param queueElement the element to look for
+    /// @return true if element is in the queue
+
+    for (auto element : this->queue_)
+    {
+      if (element.first == queueElement)
+      {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  bool insert(const T &queueElement, int priority)
+  {
+    /// @brief insert node into queue
+    /// @param queueElement the node to insert
+    /// @param priority the priority of the node
+    /// @return true if the insertion succeeded
+
+    if (this->contains(queueElement))
+    {
+      return this->setMinPrioirity(queueElement, priority);
+    }
+    else
+    {
+      for (auto it = this->queue_.begin(); it != this->queue_.end(); it++)
+      {
+        if (it->second > priority)
+        {
+          this->queue_.insert(it, pair<T, int>(queueElement, priority));
+          return true;
+        }
+      }
+    }
+    this->queue_.push_back(pair<T, int>(queueElement, priority));
+    return true;
+  }
+
+  T popTop()
+  {
+    /// @brief returns the top element of the queue and removes it.
+    /// @return the top element of the queue.
+
+    pair<T, int> element = this->queue_.front();
+    this->queue_.pop_front();
+    return element.first;
+  }
+
+  int size()
+  {
+    /// @brief get the number of queue_elements
+    /// @return number of queue_elements
+
+    return this->queue_.size();
+  };
+
+  template <class N>
+  friend ostream &operator<<(ostream &os, const PriorityQueue<T> &queue);
+
+private:
+  /// @brief each queue element is a pair of the element and its current priority
+  list<pair<T, int>> queue_;
+};
+
+template <class T>
+ostream &operator<<(ostream &os, const PriorityQueue<T> &queue)
+{
+  /// @brief Output operator for PriorityQueue<T> class
+  /// @tparam T Type of the queue elements
+  /// @param os output stream
+  /// @param queue the queue element to add to the output
+  /// @return output stream
+
+  os << "Queue with elements: " << endl;
+  for (auto element : queue.queue_)
+  {
+    os << "Priority: " << element.second << " Data: " << element.first << endl;
+  }
+  return os;
+}
+
+class TreeNode
+/// @brief class to hold information about a node in a graph and the node it can reach
+
+{
+
+public:
+  TreeNode() {}
+
+  TreeNode(const TreeNode &ele) : node_(ele.node_),
+                                  destinations_(ele.destinations_) {}
+
+  TreeNode(int node, const vector<TreeNode> &destinations) : node_(node),
+                                                             destinations_(destinations) {}
+
+  ~TreeNode() {}
+
+  inline int node() const
+  {
+    /// @brief get the source node of this element
+    /// @return node index
+
+    return this->node_;
+  }
+
+  inline vector<TreeNode> destinations() const
+  {
+    /// @brief get the reachable nodes of this element
+    /// @return list of node idx
+
+    return this->destinations_;
+  }
+
+  friend ostream &operator<<(ostream &os, const TreeNode &set);
+
+private:
+  /// @brief source node
+  int node_;
+  /// @brief reachable nodes of this element
+  vector<TreeNode> destinations_;
+};
+
+ostream &operator<<(ostream &os, const TreeNode &node)
+{
+  /// @brief Output operator for TreeNode class
+  /// @param os output stream
+  /// @param node the node element to add to the output
+  /// @return output stream
+
+  os << "(" << node.node_ << ",";
+  for (auto dest : node.destinations_)
+  {
+    os << dest;
+  }
+  os << ")";
+  return os;
+}
 
 template <class T>
 class Graph
@@ -21,6 +206,31 @@ public:
   }
 
   Graph(const Graph<T> &graph) : vertices_(graph.vertices_), edges_(graph.edges_) {}
+
+  Graph(const string filepath) : vertices_(vector<T>(0))
+  {
+    /// @brief Get graph from a file.  The file format will be an initial integer that is the
+    ///        node size of the graph and the further values will be integer triples: (i, j, cost)
+    /// @param filepath path to the file containing the graph definition
+    ifstream graphfile(filepath);
+    istream_iterator<T> start(graphfile), end;
+    int size = static_cast<int>(*start);
+    this->vertices_.resize(size);
+    for (int node = 0; node < size; node++)
+    {
+      this->vertices_[node] = node;
+      this->edges_.insert(pair<int, map<int, int>>(node, map<int, int>()));
+    }
+    vector<T> edges(++start, end);
+    for (auto edge = edges.begin(); edge != edges.end(); edge = edge + 3)
+    {
+      int i = *edge;
+      int j = *(edge + 1);
+      int cost = *(edge + 2);
+      this->addEdge(i, j);
+      this->setEdgeValue(i, j, cost);
+    }
+  }
 
   ~Graph() {}
 
@@ -168,6 +378,29 @@ public:
     return this->edges_.count(x) && this->edges_.at(x).count(y);
   }
 
+  pair<int, TreeNode> getMinimumSpanningTree() const
+  {
+    /// @brief Prim's algorithm to get minimum spanning tree
+
+    pair<int, TreeNode> result;
+    result.first = -1;
+    map<int, TreeNode> forest;
+    PriorityQueue<pair<int, int>> sortedEdges;
+    for (auto edge : this->edges_)
+    {
+      int source = edge.first;
+
+      for (auto target : edge.second)
+      {
+        int j = target.first;
+        int cost = target.second;
+        sortedEdges.insert(pair(source, target), cost);
+      }
+    }
+
+    return result;
+  }
+
   template <class N>
   friend ostream &operator<<(ostream &os, const Graph<N> &graph);
 
@@ -177,7 +410,7 @@ private:
   vector<T> vertices_;
   /*Edges are stored in a map to enablefast lookup if the starting node is given.
   - Key is the start node
-  - All nodes that arre the end point of a edge are stored in a end point map
+  - All nodes that are the end point of a edge are stored in a end point map
       - Key is the index in the end node,
       - Value is the value of the edge
   */
@@ -207,153 +440,27 @@ ostream &operator<<(ostream &os, const Graph<T> &graph)
     os << endl;
   }
   os << endl;
+  auto tree = graph.getMinimumSpanningTree();
+  os << " Minimum Spanning Tree: Cost: " << tree.first << " Tree: ";
+  os << tree.second << endl;
   return os;
 }
 
-template <class T>
-class PriorityQueue
-/// @brief The value of the PriorityQueue is to always have access to the vertex
-///        with the next shortest link in the shortest path calculation at the top of the queue.
-
-{
-public:
-  PriorityQueue() {}
-
-  ~PriorityQueue() {}
-
-  bool setMinPrioirity(const T &queueElement, int priority)
-  {
-    /// @brief changes the priority and path of a queue element to the minimum.
-    /// @param queueElement the element to change
-    /// @param priority the new priority
-    /// @return true if the update succeeded
-
-    typename list<pair<T, int>>::iterator it;
-    for (it = this->queue_.begin(); it != this->queue_.end(); it++)
-    {
-      if (it->first == queueElement)
-      {
-        if (it->second > priority)
-        {
-          it->second = priority;
-          it->first = queueElement;
-          this->queue_.sort([](pair<T, int> a, pair<T, int> b)
-                            { return a.second < b.second; });
-          return true;
-        }
-        return false;
-      }
-    }
-    return false;
-  }
-
-  int minPrioirty()
-  {
-    /// @brief priority of the top element of the queue.
-    /// @return the pririty of the top element.
-    return this->queue_.front().second;
-  }
-
-  bool contains(const T &queueElement)
-  {
-    /// @brief does the queue contains the element.
-    /// @param queueElement the element to look for
-    /// @return true if element is in the queue
-
-    for (auto element : this->queue_)
-    {
-      if (element.first == queueElement)
-      {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  bool insert(const T &queueElement, int priority)
-  {
-    /// @brief insert node into queue
-    /// @param queueElement the node to insert
-    /// @param priority the priority of the node
-    /// @return true if the insertion succeeded
-
-    if (this->contains(queueElement))
-    {
-      return this->setMinPrioirity(queueElement, priority);
-    }
-    else
-    {
-      for (auto it = this->queue_.begin(); it != this->queue_.end(); it++)
-      {
-        if (it->second > priority)
-        {
-          this->queue_.insert(it, pair<T, int>(queueElement, priority));
-          return true;
-        }
-      }
-    }
-    this->queue_.push_back(pair<T, int>(queueElement, priority));
-    return true;
-  }
-
-  T popTop()
-  {
-    /// @brief returns the top element of the queue and removes it.
-    /// @return the top element of the queue.
-
-    pair<T, int> element = this->queue_.front();
-    this->queue_.pop_front();
-    return element.first;
-  }
-
-  int size()
-  {
-    /// @brief get the number of queue_elements
-    /// @return number of queue_elements
-
-    return this->queue_.size();
-  };
-
-  template <class N>
-  friend ostream &operator<<(ostream &os, const PriorityQueue<T> &queue);
-
-private:
-  /// @brief each queue element is a pair of the element and its current priority
-  list<pair<T, int>> queue_;
-};
-
-template <class T>
-ostream &operator<<(ostream &os, const PriorityQueue<T> &queue)
-{
-  /// @brief Output operator for PriorityQueue<T> class
-  /// @tparam T Type of the queue elements
-  /// @param os output stream
-  /// @param queue the queue element to add to the output
-  /// @return output stream
-
-  os << "Queue with elements: " << endl;
-  for (auto element : queue.queue_)
-  {
-    os << "Priority: " << element.second << " Data: " << element.first << endl;
-  }
-  return os;
-}
-
-class SetElement
+class NodePath
 /// @brief class to hold information about a node in a graph and path how it is reachable
 
 {
 
 public:
-  SetElement() {}
+  NodePath() {}
 
-  SetElement(const SetElement &ele) : node_(ele.node_),
-                                      path_(ele.path_) {}
+  NodePath(const NodePath &ele) : node_(ele.node_),
+                                  path_(ele.path_) {}
 
-  SetElement(int node, const vector<int> &path) : node_(node),
-                                                  path_(path) {}
+  NodePath(int node, const vector<int> &path) : node_(node),
+                                                path_(path) {}
 
-  ~SetElement() {}
+  ~NodePath() {}
 
   inline int node() const
   {
@@ -371,9 +478,9 @@ public:
     return this->path_;
   }
 
-  friend ostream &operator<<(ostream &os, const SetElement &set);
-  friend bool operator==(const SetElement &set1, const SetElement &set2);
-  friend bool operator!=(const SetElement &set1, const SetElement &set2);
+  friend ostream &operator<<(ostream &os, const NodePath &set);
+  friend bool operator==(const NodePath &set1, const NodePath &set2);
+  friend bool operator!=(const NodePath &set1, const NodePath &set2);
 
 private:
   /// @brief destination node
@@ -382,29 +489,29 @@ private:
   vector<int> path_;
 };
 
-bool operator==(const SetElement &set1, const SetElement &set2)
+bool operator==(const NodePath &set1, const NodePath &set2)
 {
-  /// @brief Compare operator for equality of two SetElement objects
-  /// @param set1 SetElement object 1
-  /// @param set2 SetElement object 2
+  /// @brief Compare operator for equality of two NodePath objects
+  /// @param set1 NodePath object 1
+  /// @param set2 NodePath object 2
   /// @return True if the destination nodes of both elements are equal
 
   return (set1.node() == set2.node());
 }
 
-bool operator!=(const SetElement &set1, const SetElement &set2)
+bool operator!=(const NodePath &set1, const NodePath &set2)
 {
-  /// @brief Compare operator for non equality of two SetElement objects
-  /// @param set1 SetElement object 1
-  /// @param set2 SetElement object 2
+  /// @brief Compare operator for non equality of two NodePath objects
+  /// @param set1 NodePath object 1
+  /// @param set2 NodePath object 2
   /// @return True if the destination nodes of both elements are not equal
 
   return (set1.node() != set2.node());
 }
 
-ostream &operator<<(ostream &os, const SetElement &set)
+ostream &operator<<(ostream &os, const NodePath &set)
 {
-  /// @brief Output operator for Graph<T> class
+  /// @brief Output operator for NodePath class
   /// @param os output stream
   /// @param set the set element to add to the output
   /// @return output stream
@@ -539,7 +646,7 @@ private:
     /// @brief calculate the shortest path vector and the cost of this path
     /// @return True if calculation was succeeded
 
-    PriorityQueue<SetElement> openSet;
+    PriorityQueue<NodePath> openSet;
     vector<bool> closedSet = vector<bool>(this->vertices().size(), false);
     int currentCost = 0;
     vector<int> currentPath = vector<int>{this->start_};
@@ -556,10 +663,10 @@ private:
         int cost = this->graph_.getEdgeValue(nextNode, node);
         vector<int> nodePath = vector<int>(currentPath);
         nodePath.push_back(node);
-        openSet.insert(SetElement(node, nodePath), cost + currentCost);
+        openSet.insert(NodePath(node, nodePath), cost + currentCost);
       }
       currentCost = openSet.minPrioirty();
-      SetElement element = openSet.popTop();
+      NodePath element = openSet.popTop();
       currentPath = element.path();
       nextNode = element.node();
     }
@@ -631,49 +738,11 @@ Graph<string> graph_generator(int nodecount, float density, unsigned distance_ra
   return testgraph;
 }
 
-template <class T>
-class MinimumSpanningTree
-/// @brief class implementing a Kruskal minimum spanning tree algorithm
-
-{
-
-public:
-  MinimumSpanningTree(const MinimumSpanningTree<T> &tree) : graph_(tree.graph_) {}
-
-  MinimumSpanningTree(const Graph<T> &g) : graph_(g)
-  {
-    /// @param g the graph
-  }
-
-  MinimumSpanningTree() : graph_(Graph<T>()) {}
-
-  template <class N>
-  friend ostream &operator<<(ostream &os, const MinimumSpanningTree<N> &shortPath);
-
-private:
-  Graph<T> graph_;
-};
-
-template <class T>
-ostream &operator<<(ostream &os, const MinimumSpanningTree<T> &tree)
-{
-  /// @brief Output operator for Graph<T> class
-  /// @tparam T Type of the nodes of the graph
-  /// @param os output stream
-  /// @param tree the minimum spanning tree path to add to the output
-  /// @return output stream
-
-  os << endl;
-  return os;
-}
-
 int main()
 {
   // Graph with 50 nodes and 20% density
   Graph<string> testgraph = graph_generator(50, 0.2, 10);
   cout << testgraph << endl;
-  auto minTree = MinimumSpanningTree(testgraph);
-  cout << minTree << endl;
   auto shortest_path = ShortestPath<string>(testgraph, 3, 33);
   cout << shortest_path << endl;
   shortest_path.setStart(34);
@@ -682,10 +751,13 @@ int main()
   // Graph with 50 nodes and 40% density
   testgraph = graph_generator(50, 0.4, 10);
   cout << testgraph << endl;
-  minTree = MinimumSpanningTree(testgraph);
-  cout << minTree << endl;
   shortest_path = ShortestPath<string>(testgraph, 1, 6);
   shortest_path.setDestination(45);
   cout << shortest_path << endl;
+
+  // Graph from data file
+  cout << "Graph from file ../data/SampleTestData_mst_data.txt" << endl;
+  Graph<int> filegraph("../data/SampleTestData_mst_data.txt");
+  cout << filegraph << endl;
   return 0;
 }
